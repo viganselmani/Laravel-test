12e1992a4ce067b39ca5ea9aa635f4d92f9336e9
viganselmani <viganselmani.93@gmail.com>
2017-01-28 14:17:02 +0100

    Add files via upload

---
+++ screenshots/welcome.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <!-- Styles -->
+		<style type="text/css">
+.form-style-8{
+    font-family: 'Open Sans Condensed', arial, sans;
+    width: 500px;
+    padding: 30px;
+    
+    margin: 50px auto;
+  
+
+}
+.form-style-8 h2{
+    background: #4D4D4D;
+    text-transform: uppercase;
+    font-family: 'Open Sans Condensed', sans-serif;
+    color: #797979;
+    font-size: 18px;
+    font-weight: 100;
+    padding: 20px;
+    margin: -30px -30px 30px -30px;
+}
+.form-style-8 input[type="text"],
+
+.form-style-8 input[type="email"],
+
+.form-style-8 textarea
+
+{
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    outline: none;
+    display: block;
+    width: 100%;
+    padding: 7px;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    background: transparent;
+    margin-bottom: 10px;
+    font: 16px Arial, Helvetica, sans-serif;
+    height: 45px;
+}
+
+.form-style-8 input[type="button"], 
+.form-style-8 input[type="submit"]{
+   
+    background-color: #fff;
+   
+    cursor: pointer;
+    color: #000;
+    font-family: 'Open Sans Condensed', sans-serif;
+    font-size: 14px;
+    padding: 8px 18px;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+</style>
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Open sans', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            
+			.col-centered{
+display: block;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+h3{
+	color:black;
+	text-decoration: none;
+	font-weight:20px;
+}
+i
+{
+ background-color: red;
+ border-radius: 50%;
+ border: 1x solid grey;
+ padding:30px;
+}
+        </style>
+    </head>
+    <body>
+     <div class="container-fluid">
+   <div class="row">
+	<img style="max-width: 100%; border: 12px solid white;" src="http://chamberssoft.com/wp-content/uploads/2015/02/web-development-banner.png" class="img-responsive">
+   </div>
+</div>
+<div class="container">
+<div class="col-lg-11 col-centered">   
+<h3>Brands that trust us</h3>
+<h5 style="margin-top:-8px;">It doesn't matter if you are a startup or an international reknown company. We work with everyone</h5>
+
+</div>
+ 
+
+</div>
+   <div class="row" style="width: 80%; text-align:center; margin-right: auto; margin-left: auto;">
+    <div class="col-sm-4"><img src="http://kassdirektor.cdn4.shoprenter.hu/custom/kassdirektor/image/data/Cornavin%20/logo_962.jpg"></div>
+    <div class="col-sm-4"><img src="http://route66ventures.com/wp-content/uploads/2015/10/Knip-Logo.png"></div>
+    <div class="col-sm-4"><img style="margin-top:50px;" src="http://www.startfeld.ch/wp-content/uploads/2016/08/ipaster-logo-300x169.png"></div>
+  </div>     
+   
+   <div class="philosophy">
+<div class="col-lg-11 col-centered">   
+<h3>Our philosophy</h3>
+<h5>It doesn't matter if you are a startup or an international reknown company. We work with everyone</h5>
+</div>
+<div class="row" style="background-color:#CCCCCC; width:100%; text-align:center; margin-right: auto; margin-left: auto; border: 12px solid white;">
+    <div class="col-sm-4">
+	<img src="img/3icons.png" width="100px">
+	<h5 style="color:black;"><b>Various Categories</b></h5>
+	<p>We always have the solution you need.</p>
+	</div>
+    <div class="col-sm-4"><img src="img/1icon.png" width="100px">
+	<h5 style="color:black;"><b>Experienced Developers</b></h5>
+	<p>Alltogether we have more than 500 year of experience</p>
+	</div>
+    <div class="col-sm-4"><img src="img/2icons.png" width="100px">
+	<h5 style="color:black;"><b>Good work & Discipline</b></h5>
+	<p>We really care about your software!</p>
+	</div>
+  </div>  
+<div class="col-lg-11 col-centered" style="background-color:#111d28; width:100%; text-align:center; margin-right: auto; margin-left: auto; border: 12px solid white;">
+<h3 style="color:white;">Contact us</h3>
+<div class="form-style-8">
+  
+  <form>
+    <input type="text" name="field1" placeholder="Name" />
+    <input type="email" name="field2" placeholder="Email" />
+    <input type="text" name="field3" placeholder="Subject" />
+    
+    <input type="button" value="Send" />
+  </form>
+</div>
+  
+</div>
+
+<div class="col-lg-11 col-centered">
+<h2 style="color:black;">THE/CODERS</h2>
+<a href="#">Terms & Privacy</a> | &copy 2016 - SuccessField Influencers, All rights reserved.
+</div>
+</div>
+    </body>
+</html>
